df49216fad71da7f4a9e054d1323623cf24d9788
Per Rawdin <pr222iw@student.lnu.se>
2021-04-04 18:50:54 +0200

    Add sign token pairs method

--- src/resolvers/users.js
+++ src/resolvers/users.js
@@ -8,6 +8,9 @@
 // import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import { User } from '../models/user.js'
+import fs from 'fs'
+import redis from 'redis'
+import { promisify } from 'util'
 
 // Provide resolver functions for your schema fields
 const resolvers = {
@@ -51,6 +54,47 @@ const resolvers = {
     // TODO: Validate user data
     // TODO: Make sure user doesnt already exist
     // TODO: Hash password and create access token
+  }
+}
+
+/**
+ * Signs a token pair of JWT access and refresh tokens.
+ *
+ * @param {object} user - User object.
+ * @returns {object} A object including the tokens.
+ */
+const _signTokenPairs = (user) => {
+  try {
+    const privateKey = fs.readFileSync(process.env.KEY_PATH, 'utf8')
+
+    // Create the access token with the shorter lifespan.
+    const accessToken = jwt.sign({ sub: user.id }, privateKey, {
+      algorithm: 'RS256',
+      expiresIn: process.env.ACCESS_TOKEN_LIFE
+    })
+
+    // Create the refresh token with the longer lifespan.
+    const refreshToken = jwt.sign({ sub: user.id }, process.env.REFRESH_TOKEN_SECRET, {
+      algorithm: 'HS256',
+      expiresIn: process.env.REFRESH_TOKEN_LIFE
+    })
+
+    // Save the refresh token in Redis.
+    const redisClient = redis.createClient({
+      password: process.env.REDIS_SECRET
+    })
+
+    redisClient.set(user.id, refreshToken)
+    redisClient.expire(user.id, process.env.REDIS_TOKEN_LIFE)
+
+    redisClient.quit()
+
+    return {
+      access_token: accessToken,
+      refresh_token: refreshToken
+    }
+  } catch (error) {
+    throw new Error('Token signing error')
   }
 }
 
